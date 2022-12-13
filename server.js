182b7eb3e9e354e3fcef505b98b85414aaf7ceaa
imran <mdimranfullstackdev@gmail.com>
2022-12-13 11:29:57 +0530

    raftlab-backend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 require("dotenv").config();
 const cors = require("cors");
-app.use(cors());
+app.use( cors({ origin: `${process.env.CLIENT_URL}`, methods: "GET,POST,PUT,DELETE", credentials: true, }) );
 app.use(express.json());
 const bodyParser = require("body-parser");
 
